56b21b1a1f76537bdaa00367219528c4c8af749e
Devipriya R <devipriya962006@gmail.com>
2026-01-22 17:59:22 +0530

    Time: 1 ms (99.84%) | Memory: 47.2 MB (21.29%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int uniq=0;
+        for(int i=0;i<nums.length;i++){
+            uniq=uniq^nums[i];
+        }
+        return uniq;
+        
+    }
+}
