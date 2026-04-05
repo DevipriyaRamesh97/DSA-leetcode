523267b3c7f0c42776691aafb266631703927001
Devipriya R <devipriya962006@gmail.com>
2026-04-05 21:15:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (39.50%) - LeetSync

--- 2491-smallest-even-multiple/smallest-even-multiple.java
+++ 2491-smallest-even-multiple/smallest-even-multiple.java
@@ -2,9 +2,8 @@ class Solution {
     public int smallestEvenMultiple(int n) {
         if(n%2==0){
             return n;
-        }else{
-            return 2*n;
         }
+        return 2*n;
         
     }
 }
