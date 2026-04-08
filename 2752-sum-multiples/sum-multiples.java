e21972be83e714687fa7bd8ed17ebcf33d976c56
Devipriya R <devipriya962006@gmail.com>
2026-04-08 21:49:01 +0530

    Time: 2 ms (97.70%) | Memory: 42.4 MB (90.91%) - LeetSync

--- 2752-sum-multiples/sum-multiples.java
+++ 2752-sum-multiples/sum-multiples.java
@@ -1,12 +1,12 @@
 class Solution {
     public int sumOfMultiples(int n) {
         int sum=0;
-        for(int i=1;i<=n;i++){
+       for(int i=1;i<=n;i++){
             if(i%3==0 || i%5==0 || i%7==0){
-                sum=sum+i;
-            }
+                sum += i;
         }
-        return sum;
+       }
+       return sum;
         
     }
 }
